6fcf50e1ba89bb4ff759a898558298c5f9e83ae5
hzxiebao <hzxiebao@ceboshi.cn>
2026-03-12 18:59:29 +0800

    Update UI: simplify homepage, remove custom nav bars, update form layout with each field on its own row

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,18 +1,5 @@
 Page({
-  data: {
-    nickname: '',
-  },
-
   onShow() {
-    const { nickname } = getApp().globalData;
-    this.setData({ nickname });
-  },
-
-  goProfile() {
-    wx.switchTab({ url: '/pages/profile/profile' });
-  },
-
-  goPetList() {
-    wx.navigateTo({ url: '/pages/pet-list/pet-list' });
+    // 无需获取个人信息
   },
 });
